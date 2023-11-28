dbf15a9811413b291042a824fce8323b9e3390a3
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-11-29 00:49:57 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"> 
+    <ApiData />
+  </div>
+</template>
+
+<script>
+import ApiData from "./components/ApiData.vue"
+
+
+export default {
+  components:{
+    ApiData,
+  },
+};
+</script>
